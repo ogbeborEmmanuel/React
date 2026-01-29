2e6f0180f37a5b6abe5779856f7ec50af8639e92
ogbebor emmanuel <ogbeboremmanuel917@gmail.com>
2026-01-29 10:17:10 +0100

    updated list

--- src/Component/PackingList.js
+++ src/Component/PackingList.js
@@ -6,6 +6,18 @@ const PackingList = ({ items, setItems }) => {
     const numberOfItems = items.length;
     const numberOfPackedItems = items.filter(item => item.packed).length;
     const percentage = Math.round((numberOfPackedItems / numberOfItems) * 100)
+
+
+    const [sortBy, setSortBy] = React.useState("input");
+    let sortedItems;
+
+    const handleClearList = () => {
+        setItems([]);
+    }
+
+    if (sortBy === "input") sortedItems = items;
+    if (sortBy === "description") sortedItems = items.slice().sort((a, b) => a.description.localeCompare(b.description));
+    if (sortBy === "packed") sortedItems = items.slice().sort((a, b) => Number(a.packed) - Number(b.packed));
     const deleteTask = item => {
         const filteredList = items.filter(task => task.id !== item.id);
         setItems(filteredList);
@@ -16,10 +28,26 @@ const PackingList = ({ items, setItems }) => {
         console.log(items);
 
     }
+    console.log(sortBy);
     return (
         <>
-            <div>{items.map(item => <div style={item.packed ? { textDecoration: "line-through" } : {}} key={item.id}><input type="checkbox" value={item.packed} onChange={() => handleToggle(item.id)} />{item.description} {item.quatity}  <button onClick={() => deleteTask(item)}>X</button></div>)}</div>
+            <div>{sortedItems.map(item => <div style={item.packed ? { textDecoration: "line-through" } : {}} key={item.id}><input type="checkbox" value={item.packed} onChange={() => handleToggle(item.id)} />{item.description} {item.quatity}  <button onClick={() => deleteTask(item)}>X</button></div>)}</div>
             <Stats numberOfItems={numberOfItems} numberOfPackedItems={numberOfPackedItems} percentage={percentage} />
+            <div className='actions'>
+                <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+                    <option value="input">
+                        sort by input order
+                    </option>
+                    <option value="description">
+                        sort by description
+                    </option>
+                    <option value="packed">
+                        sort by packed status
+                    </option>
+                </select>
+            </div>
+
+            <button onClick={handleClearList}>Clear List</button>
         </>
 
     )
